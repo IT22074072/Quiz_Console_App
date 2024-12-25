240807cd0f712697e01febd6d77be03f1371accf
IT22074072 <it22074072@my.sliit.lk>
2024-12-25 18:18:13 +0530

    play quiz

--- QuestionService.java
+++ QuestionService.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class QuestionService {
     Question[] questions = new Question[5];
 
+    String selection[] = new String[5];
     public QuestionService() {  //constructor
         questions[0] = new Question(1, "Number of members in BTS?", "5", "7", "9", "4", "7");
         questions[1] = new Question(2, "Number of members in BTS?", "5", "7", "9", "4", "7");
@@ -12,9 +15,26 @@ public class QuestionService {
 
     }
 
-    public void displayQuestions() {
+    int i = 0;
+    public void playQuiz() {
         for (Question q : questions) {
-            System.out.println(q);
+            System.out.println("Question no. "+q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your answer:  ");
+            selection[i] = sc.nextLine();
+            i++;
+            System.out.println();
+        }
+
+        System.out.println();
+        
+        for (String s : selection) {
+            System.out.println(s);
         }
     }
 }
